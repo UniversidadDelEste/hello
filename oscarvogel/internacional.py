825999540fd31d581bbc9033cb426866552928f1
Mariano Reingart <reingart@gmail.com>
2015-10-16 18:18:21 -0300

    Correcciones:
     * crear la estructura correcta para localizaciones (lenguaje/LC_MESSAGES)
     * compilar el mensaje con
     * ajustes en el código para ubicar correctamente la traducción

--- oscarvogel/internacional.py
+++ oscarvogel/internacional.py
@@ -1,9 +1,23 @@
-
+# coding: utf-8
+import os
 import gettext
-gettext.bindtextdomain('internacional', './po')
+import locale
+
+# primero, establecer que configuración de internacionalización se utiliza:
+
+print locale.setlocale(locale.LC_ALL, "");
+
+# luego, ubicar la carpeta de localizaciones (con la estructura correcta):
+
+localedir = os.path.abspath(os.path.join(os.path.dirname(__file__), 'locales'))
+print "localedir", localedir
+
+gettext.bindtextdomain('internacional', localedir)
 gettext.textdomain('internacional')
 
 _ = gettext.gettext
 
-print gettext.find('internacional')
-print _('Hello World')
+print gettext.find('internacional', localedir, None, True)
+
+# el mensaje a traducir debe ser escrito tal cual 
+print _('hello, world')     
